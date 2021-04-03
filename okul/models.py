ce85f58d81303164b0ec681d94d25e19799c41d2
Elif BAŞ <elif.bas8@gmail.com>
2021-04-03 20:01:01 +0300

    il eklendi

--- okul/models.py
+++ okul/models.py
@@ -11,3 +11,14 @@ class Okul(models.Model):
 
     def __str__(self):
         return self.baslik  ##her objenin adı baslık değişkeninde ki değer olarak görünsün.
+
+class İl(models.Model):
+    baslik = models.CharField(max_length=120, verbose_name='Başlık') #charfield (karakter alani) icin maximum boyutu mutlaka belirtmeliyiz
+    metin = models.TextField(verbose_name='Metin') # textfield çok uzun yazilar için kullanir, boyut belirtmeye gerek yok
+    yayinlanmaTarihi = models.DateTimeField(verbose_name='Yayınlanma Tarihi') #tarih ve saat bilgisini alir.
+
+#verbose_name kullanıcaya nasıl görünmesini istiyorsun
+
+
+    def __str__(self):
+        return self.baslik  ##her objenin adı baslık değişkeninde ki değer olarak görünsün.
