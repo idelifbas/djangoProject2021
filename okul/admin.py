77feb981a80f75ea3cf0898ca106039cd57f7996
Elif BAŞ <elif.bas8@gmail.com>
2021-04-03 19:18:29 +0300

    django case

---
+++ okul/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from okul.models import Okul
+# Register your models here.
+admin.site.register(Okul)
---
+++ okul/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Okul(models.Model):
+    baslik = models.CharField(max_length=120, verbose_name='Başlık') #charfield (karakter alani) icin maximum boyutu mutlaka belirtmeliyiz
+    metin = models.TextField(verbose_name='Metin') # textfield çok uzun yazilar için kullanir, boyut belirtmeye gerek yok
+    yayinlanmaTarihi = models.DateTimeField(verbose_name='Yayınlanma Tarihi') #tarih ve saat bilgisini alir.
+
+#verbose_name kullanıcaya nasıl görünmesini istiyorsun
+
+
+    def __str__(self):
+        return self.baslik  ##her objenin kendi adı görünsün
